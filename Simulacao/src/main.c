b54078ee84fdb0f5ecc2a19e171097160483b860
Lucas Emanoel <lucaasifx@gmail.com>
2025-02-02 10:22:25 -0300

    :sparkles: feat: Implementação do temporizador do semáforo

--- Simulacao/src/main.c
+++ Simulacao/src/main.c
@@ -15,11 +15,41 @@
 // específico para a simulação
 #define LED_YELLOW 1
 
-volatile uint8_t cycle = 0;
+// responsável por controlar o sinal atual
+    // inicialmente vermelho
+volatile uint8_t cycle = 1;
 
 // Função de repeating
-bool repeating_timer_callback() {
-    
+bool repeating_timer_callback(struct repeating_timer *t) {
+    switch (cycle) {
+        // sinal vermelho
+        case 1:
+            turn_led_off(LED_YELLOW);
+            turn_led_off(LED_GREEN);
+            turn_led_on(LED_RED);
+            printf("Sinal Vermelho\n");
+            cycle++;
+            break;
+        // sinal amarelo
+        case 2:
+            turn_led_off(LED_RED);
+            turn_led_off(LED_GREEN);         
+            turn_led_on(LED_YELLOW);
+            printf("Sinal Amarelo\n");
+            cycle++;
+            break;
+        // sinal verde
+        case 3:
+            turn_led_off(LED_RED);
+            turn_led_off(LED_YELLOW);         
+            turn_led_on(LED_GREEN);
+            printf("Sinal Verde\n\n");
+            cycle = 1;
+            break;
+        default:
+            break;
+    }
+    return true;
 }
 
 
@@ -29,6 +59,16 @@ int main() {
     init_led(LED_RED);
     init_led(LED_YELLOW);   
     init_led(LED_GREEN);
+
+    // leds começam desligados
+    turn_led_off(LED_RED);
+    turn_led_off(LED_YELLOW);
+    turn_led_off(LED_GREEN);
+
+    // início do ciclo
+    turn_led_on(LED_RED);
+    printf("Sinal Vermelho\n");
+    cycle++;
 
     // nosso temporizador de repetição
     struct repeating_timer timer;
